db008c615afbdca1c15f0f4de21b1e22f2213f9a
Anderson Sprenger <anderson.sprenger@edu.pucrs.br>
2020-07-01 17:30:22 -0300

    init

---
+++ app/src/main/java/br/poa/andersonsprenger/alcoolougasolina/MainActivity.java
@@ -0,0 +1,36 @@
+package br.poa.andersonsprenger.alcoolougasolina;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void calcular(View view){
+        TextInputEditText precoAlcool       = findViewById(R.id.precoAlcool);
+        TextInputEditText precoGasolina     = findViewById(R.id.precoGasolina);
+        TextView textoCalculo               = findViewById(R.id.textoCalculo);
+
+        try {
+            if (
+                    Double.parseDouble(precoAlcool.getText().toString())   /
+                    Double.parseDouble(precoGasolina.getText().toString()) <
+            0.7d){
+                textoCalculo.setText("Melhor abastecer com alcool.");
+            } else {
+                textoCalculo.setText("Melhor abastecer com gasolina.");
+            }
+        } catch (NumberFormatException e){
+            textoCalculo.setText("=( Ops, algo não deu certo! " + e.getMessage());
+        }
+    }
+}
